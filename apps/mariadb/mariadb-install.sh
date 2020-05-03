37fbcdcff5fc8fc11dafb578199339450aabfd67
Francisco <flara.commercial@gmail.com>
2020-05-02 23:28:05 -0400

    add the install script, pv, pvc and helm values

--- apps/mariadb/mariadb-install.sh
+++ apps/mariadb/mariadb-install.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-helm show values bitnami/mariadb >> mariadb.values.yml
-helm install my-release bitnami/mariadb
+#create mariadb namespace
+kubectl create namespace mariadb
+
+#create persisten volumes
+kubectl apply -f mariadb.persistentvolume.yml
+kubectl get pv
+
+#create persistent volume claim
+kubectl apply -f mariadb.persistentvolumeclaim.yml
+kubectl get pvc -n mariadb
+
+helm install mariadb bitnami/mariadb --namespace mariadb --values mariadb.values.yml
+
+kubectl get pods -n mariadb -o wide
